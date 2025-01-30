fc4107ea5ac92e0474fd70964294db0e787af7b8
Jacob Tallio <pg27jacob@vfs.com>
2025-01-30 09:28:59 -0800

    PUSH: push 1

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private Vector2 _movement2D;
+
+    public void OnMove(InputValue value)
+    {
+        
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        Vector2 right = Vector2.right;
+        Vector2 left = Vector2.left;
+        
+    }
+}
